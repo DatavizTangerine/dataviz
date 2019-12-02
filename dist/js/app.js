4ffb520e5d0b6159cf1753cb66d3ae0f231049bb
Osmiios <hugo.lefrant@gmail.com>
2019-12-02 10:41:12 +0100

    Feature: Cursor & fadeIn introduction

--- dist/js/app.js
+++ dist/js/app.js
@@ -98,6 +98,18 @@ eval("__webpack_require__.r(__webpack_exports__);\nvar chooseSex = function choo
 
 /***/ }),
 
+/***/ "./js/components/cursor.js":
+/*!*********************************!*\
+  !*** ./js/components/cursor.js ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar cursor = function cursor() {\n  var cursor = document.getElementById('cursor');\n  document.addEventListener('mousemove', function (e) {\n    var x = e.clientX;\n    var y = e.clientY;\n    cursor.style.left = x + \"px\";\n    cursor.style.top = y + \"px\";\n  });\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (cursor); // document.addEventListener('click', () => {\n//     cursor.classList.add(\"expand\");\n//\n//     setTimeout(() => {\n//         cursor.classList.remove(\"expand\");\n//     }, 500)\n// })\n\n//# sourceURL=webpack:///./js/components/cursor.js?");
+
+/***/ }),
+
 /***/ "./js/components/introduction.js":
 /*!***************************************!*\
   !*** ./js/components/introduction.js ***!
@@ -106,7 +118,7 @@ eval("__webpack_require__.r(__webpack_exports__);\nvar chooseSex = function choo
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\nvar introduction = function introduction() {\n  var btn = document.querySelector('.introduction span');\n\n  if (btn) {\n    var sectionIntroduction = document.querySelector('.introduction');\n    var sectionOnboarding = document.querySelector('.onboarding');\n    btn.addEventListener('click', function () {\n      sectionIntroduction.classList.add('fadeOut');\n      sectionOnboarding.classList.add('fadeIn');\n    });\n  }\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (introduction);\n\n//# sourceURL=webpack:///./js/components/introduction.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nvar introduction = function introduction() {\n  var btn = document.querySelector('.introduction p');\n\n  if (btn) {\n    var sectionIntroduction = document.querySelector('.introduction');\n    var sectionOnboarding = document.querySelector('.onboarding');\n    btn.addEventListener('click', function () {\n      sectionIntroduction.classList.add('slideOff');\n      sectionOnboarding.classList.add('slideOn');\n    });\n  }\n\n  var text = document.querySelectorAll('.introduction .content .text');\n  text[0].classList.add('is-active');\n  var word = text[0].querySelectorAll('span');\n  word[word.length - 1].addEventListener('transitionend', function () {\n    text[0].classList.remove('is-active');\n    text[1].classList.add('is-active');\n    word = text[1].querySelectorAll('span');\n    word[word.length - 1].addEventListener('transitionend', function () {\n      text[1].classList.remove('is-active');\n      text[2].classList.add('is-active');\n    });\n  });\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (introduction);\n\n//# sourceURL=webpack:///./js/components/introduction.js?");
 
 /***/ }),
 
@@ -118,7 +130,19 @@ eval("__webpack_require__.r(__webpack_exports__);\nvar introduction = function i
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\nvar onboarding = function onboarding() {\n  var btn = document.querySelector('.onboarding span');\n\n  if (btn) {\n    var sectionOnboarding = document.querySelector('.onboarding');\n    var sectionChoose = document.querySelector('.choose__sexe');\n    btn.addEventListener('click', function () {\n      sectionOnboarding.classList.add('fadeOut');\n      sectionOnboarding.classList.remove('fadeIn');\n      sectionChoose.classList.add('fadeIn');\n    });\n  }\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (onboarding);\n\n//# sourceURL=webpack:///./js/components/onboarding.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nvar onboarding = function onboarding() {\n  var btn = document.querySelector('.onboarding span');\n\n  if (btn) {\n    var sectionOnboarding = document.querySelector('.onboarding');\n    var sectionChoose = document.querySelector('.choose__sexe');\n    var timeline = document.querySelector('.timeline');\n    var topLine = document.querySelector('.line-top');\n    btn.addEventListener('click', function () {\n      sectionOnboarding.classList.add('slideOff');\n      sectionOnboarding.classList.remove('slideOn');\n      sectionChoose.classList.add('slideOn');\n      timeline.classList.add('show');\n      topLine.classList.remove('hidden');\n    });\n  }\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (onboarding);\n\n//# sourceURL=webpack:///./js/components/onboarding.js?");
+
+/***/ }),
+
+/***/ "./js/components/timeline.js":
+/*!***********************************!*\
+  !*** ./js/components/timeline.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar timeline = function timeline() {\n  var timeline = document.querySelector('.timeline');\n  var items = timeline.querySelectorAll('.timeline__item');\n  items.forEach(function (item) {\n    item.addEventListener('click', function () {\n      items.forEach(function (item) {\n        item.classList.remove('is-active');\n      });\n      item.classList.add('is-active');\n    });\n  });\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (timeline);\n\n//# sourceURL=webpack:///./js/components/timeline.js?");
 
 /***/ }),
 
@@ -130,7 +154,7 @@ eval("__webpack_require__.r(__webpack_exports__);\nvar onboarding = function onb
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_introduction__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/introduction */ \"./js/components/introduction.js\");\n/* harmony import */ var _components_onboarding__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/onboarding */ \"./js/components/onboarding.js\");\n/* harmony import */ var _components_chooseSexe__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/chooseSexe */ \"./js/components/chooseSexe.js\");\n\n\n\ndocument.addEventListener(\"DOMContentLoaded\", function () {\n  Object(_components_introduction__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n  Object(_components_onboarding__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n  Object(_components_chooseSexe__WEBPACK_IMPORTED_MODULE_2__[\"default\"])();\n});\n\n//# sourceURL=webpack:///./js/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_introduction__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/introduction */ \"./js/components/introduction.js\");\n/* harmony import */ var _components_onboarding__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/onboarding */ \"./js/components/onboarding.js\");\n/* harmony import */ var _components_chooseSexe__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/chooseSexe */ \"./js/components/chooseSexe.js\");\n/* harmony import */ var _components_timeline__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/timeline */ \"./js/components/timeline.js\");\n/* harmony import */ var _components_cursor__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./components/cursor */ \"./js/components/cursor.js\");\n\n\n\n\n\ndocument.addEventListener(\"DOMContentLoaded\", function () {\n  Object(_components_introduction__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n  Object(_components_onboarding__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\n  Object(_components_chooseSexe__WEBPACK_IMPORTED_MODULE_2__[\"default\"])();\n  Object(_components_timeline__WEBPACK_IMPORTED_MODULE_3__[\"default\"])();\n  Object(_components_cursor__WEBPACK_IMPORTED_MODULE_4__[\"default\"])();\n});\n\n//# sourceURL=webpack:///./js/main.js?");
 
 /***/ }),
 
@@ -152,7 +176,7 @@ eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./scss
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("__webpack_require__(/*! J:\\wamp64\\www\\dataviz\\src/js/main.js */\"./js/main.js\");\nmodule.exports = __webpack_require__(/*! J:\\wamp64\\www\\dataviz\\src/scss/main.scss */\"./scss/main.scss\");\n\n\n//# sourceURL=webpack:///multi_./js/main.js_./scss/main.scss?");
+eval("__webpack_require__(/*! D:\\wamp64\\www\\dataviz\\src/js/main.js */\"./js/main.js\");\nmodule.exports = __webpack_require__(/*! D:\\wamp64\\www\\dataviz\\src/scss/main.scss */\"./scss/main.scss\");\n\n\n//# sourceURL=webpack:///multi_./js/main.js_./scss/main.scss?");
 
 /***/ })
 
--- src/js/components/introduction.js
+++ src/js/components/introduction.js
@@ -1,12 +1,28 @@
 const introduction = () => {
-    const btn = document.querySelector('.introduction span');
+    const btn = document.querySelector('.introduction p');
     if (btn){
         const sectionIntroduction = document.querySelector('.introduction');
         const sectionOnboarding = document.querySelector('.onboarding');
         btn.addEventListener('click', () => {
-            sectionIntroduction.classList.add('fadeOut');
-            sectionOnboarding.classList.add('fadeIn');
+            sectionIntroduction.classList.add('slideOff');
+            sectionOnboarding.classList.add('slideOn');
         })
     }
+
+    const text = document.querySelectorAll('.introduction .content .text');
+    text[0].classList.add('is-active');
+    let word = text[0].querySelectorAll('span');
+        word[word.length - 1].addEventListener('transitionend', () =>{
+            text[0].classList.remove('is-active');
+
+            text[1].classList.add('is-active');
+            word = text[1].querySelectorAll('span');
+
+            word[word.length - 1].addEventListener('transitionend', () =>{
+                text[1].classList.remove('is-active');
+
+                text[2].classList.add('is-active');
+            })
+        })
 }
 export default introduction;
